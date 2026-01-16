c65f646429e25f9c9d8b092cf20fb5c8ad961cce
Samuel Kim <sammy03312@gmail.com>
2026-01-15 18:28:20 -0800

    Add IOS budgeting app frontend

---
+++ frontend/LikeLionBudget/LikeLionBudget/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  LikeLionBudget
+//
+//  Created by samuel kim on 1/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
